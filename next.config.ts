a7bf5c4d5137281ae49fd81b03b1414147150e41
Vannydes <vanny.sto78@gmail.com>
2025-11-21 18:36:30 +0100

    fix build per Vercel - rimuovo temporaneamente old pwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import nextPWA from "@ducanh2912/next-pwa";
+const nextConfig = {
+  // Qui aggiungeremo dopo il nuovo plugin PWA migliore
+};
 
-const withPWA = nextPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
-export default withPWA({
-  turbopack: {},   // <--- aggiungi solo questa riga
-});
+export default nextConfig;
